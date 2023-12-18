b4f408785548585afe65ed1e9b4ca16fd30005e4
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-12-18 07:38:51 -0300

    voltando corsheaders

--- src/ponto2/settings.py
+++ src/ponto2/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
-    # 'corsheaders',
+    'corsheaders',
     'drf_spectacular',
 
     'bordado',
@@ -79,7 +79,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
