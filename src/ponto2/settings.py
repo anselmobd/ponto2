f4949bbd22698bc579930ed9173e40f68dd2b5d4
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-12-18 08:07:36 -0300

    desespero CORS

--- src/ponto2/settings.py
+++ src/ponto2/settings.py
@@ -160,13 +160,17 @@ SIMPLE_JWT = {
 }
 
 
-CORS_ALLOW_HEADERS = "*"
-CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-#     "http://localhost:88",
-# ]
 CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = "*"
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:88",
+]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    "http://localhost:5173",
+    "http://localhost:88",
+]
 
 
 SPECTACULAR_SETTINGS = {
