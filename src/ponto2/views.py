e751882b06c2c86485f722a5edb8fd127eec41d0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-12-19 19:32:40 -0300

    "home" chama principal

--- src/ponto2/views.py
+++ src/ponto2/views.py
@@ -1,10 +1,11 @@
 from pprint import pprint
 
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 
 __all__ = ['index']
 
 
 def index(request):
-    return redirect('bordado:index')
+    # return redirect('bordado:index')
+    return render(request, 'principal.html', {})
