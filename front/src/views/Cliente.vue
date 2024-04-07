168a80c6fb9f0ccb1bd3a79babd46d6bfc86f8b0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-04-06 21:31:01 -0300

    Evoluindo Cliente.vue

--- front/src/views/Cliente.vue
+++ front/src/views/Cliente.vue
@@ -2,7 +2,7 @@
 import router from '@/router'
 import { useRoute } from "vue-router";
 import { ref, onMounted } from 'vue'
-import { getCliente } from '../api/cliente.js';
+import { getCliente, putCliente } from '../api/cliente.js';
 
 const route = useRoute();
 
@@ -34,6 +34,24 @@ function doGetCliente(callBack) {
   });
 }
 
+function cbSaveCliente(data, error) {
+  if (data) {
+    cliente.value = data;
+  }
+  if (error) {
+    lancamento.value.error = error.response.data.human.join('|');
+    lancamento.value.error_tech = error.response.data.tech.join('|');
+  };
+}
+
+function doSaveCliente(callBack) {
+  putCliente({
+    id: route.params.id,
+    payload: cliente.value,
+    callBack: cbSaveCliente
+  });
+}
+
 // Lifecycle Hooks
 
 onMounted(() => {
@@ -44,8 +62,29 @@ onMounted(() => {
 
 <template>
   <div>
-    <span v-if="cliente_carregando">Carregando dados dos pedidos...</span><br>
+    <span v-if="cliente_carregando">Carregando dados do cliente {{ route.params.id }}</span>
+    <span v-if="cliente_error" class="text-red-800">{{ cliente_error }}</span>
+    <section id="titulo" class="flex pt-4 place-content-between">
+      <h2 class="inline font-bold text-xl">Dados do cliente <span v-if="cliente?.apelido" class="text-indigo-700">{{ cliente?.apelido }}</span></h2>
+      <a title="Voltar" class="button text-xl cursor-pointer" @click.prevent="router.go(-1)">&#x2190;</a>
+    </section>
     <p v-if="cliente?.id">cliente={{ cliente }}</p>
-    <span v-if="cliente_error" class="text-red-800">{{ cliente_error }}</span><br>
+    <form @submit.prevent="doSaveCliente()">
+      <p class="my-4">
+        <label for="apelido">Apelido:</label>
+        <input
+          class="mx-0.5 border border-solid border-slate-500 rounded"
+          v-model="cliente.apelido"
+          type="text"
+          name="apelido"
+          id="apelido"
+          required
+        >
+      </p>
+      <button
+        class="px-2 py-1 rounded-xl bg-sky-700 font-bold text-slate-100"
+        type="submit"
+      >Salvar</button>
+    </form>
   </div>
 </template>
