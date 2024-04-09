5de8fb1d62191f395074e167dcd244b982197d8e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-04-09 12:21:14 -0300

    criado PedidoItemDownSerializer

---
+++ src/bordado/serializers/down/pedido_item.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from bordado.models import (
+    PedidoItem,
+)
+from bordado.serializers.down.bordado import BordadoDownSerializer
+from bordado.serializers.down.pedido import PedidoDownSerializer
+from bordado.serializers.simple.user import UserSimpleSerializer
+
+
+__all__ = [
+    'PedidoItemDownSerializer',
+]
+
+
+class PedidoItemDownSerializer(serializers.ModelSerializer):
+    pedido = PedidoDownSerializer()
+    bordado = BordadoDownSerializer()
+    usuario = UserSimpleSerializer()
+
+    class Meta:
+        model = PedidoItem
+        fields = [
+            'id',
+            'pedido',
+            'ordem',
+            'inserido_em',
+            'bordado',
+            'quantidade',
+            'preco',
+            'programacao',
+            'ajuste',
+            'cancelado',
+            'usuario',
+            # 'ordemproducao'  # Não utilizado por ora
+        ]
