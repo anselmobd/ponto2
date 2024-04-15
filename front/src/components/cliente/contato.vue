bc16b08be8e7e72ec935e2747e1c420ee9047da7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-04-15 18:13:45 -0300

    evoluindo funcionalidade

--- front/src/components/cliente/contato.vue
+++ front/src/components/cliente/contato.vue
@@ -1,25 +1,41 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, watch, nextTick, onMounted } from 'vue';
 import { delContato } from '../../api/contato.js';
 
 const status = ref('b'); // browsing inserting editing
 
 const contato = ref({});
 const contato_erro = ref({});
+const acoes_mensagem = ref('');
 
 const props = defineProps({
   contato_set: Array
 });
 
+// componentes do template que serão referenciados
+
+const inputNome = ref(null)
+
 // get set refs
 
-function clearInputs() {
+function setInputs({
+  id=-1,
+  nome='',
+  telefone='',
+  email='',
+  preferencial=false
+}) {
   contato.value = {
-    nome: '',
-    telefone: '',
-    email: '',
-    preferencial: false
-  }
+    id: id,
+    nome: nome,
+    telefone: telefone,
+    email: email,
+    preferencial: preferencial
+  };
+}
+
+function clearInputs() {
+  setInputs({});
 }
 
 function clearErrors() {
@@ -31,6 +47,16 @@ function clearErrors() {
   }
 }
 
+function clearAcoesMensagem() {
+  acoes_mensagem.value = '';
+}
+
+function clearAll() {
+  clearInputs();
+  clearErrors();
+  clearAcoesMensagem();
+}
+
 // generic functions
 
 function apagaContatoNaTela(index) {
@@ -40,61 +66,109 @@ function apagaContatoNaTela(index) {
 // DB API calls (do) and callbacks (cb)
 
 function cbDelContato(index) {
-  if (index != -1) {
+  if (index == -1) {
+    acoes_mensagem.value = "Erro ao tentar apagar contato";
+  } else {
     apagaContatoNaTela(index);
   }
 }
 
 function doDelContato(index, id) {
-  delContato(
-    index,
-    id,
-    cbDelContato
-  );
+  delContato(index, id, cbDelContato);
+}
+
+function cbAddLancamento(data, error) {
+  if (data) {
+    status.value = 'b';
+    clearLancamento();
+    doGetLancamentos(1);
+  }
+  if (error) {
+    lancamento.value.error = error.response.data.human.join('|');
+    lancamento.value.error_tech = error.response.data.tech.join('|');
+  };
+}
+
+function doAddLancamento(callBack) {
+  const payload= {
+    "cliente": {
+      "apelido": route.params.apelido,
+    },
+    "data": lancamento.value.data,
+    "informacao": lancamento.value.informacao,
+    "valor": lancamento.value.valor,
+  }
+  addLancamento({
+    payload: payload,
+    callBack: cbAddLancamento
+  });
+}
+
+function cbAddContato() {
+
+}
+
+function doAddContato() {
+
+}
+
+function inputNomeFocus() {
+  nextTick(() => {
+    inputNome.value.focus();
+  })
 }
 
 // event functions
 
 function handleNovoClick(event) {
   event.preventDefault();
-  clearInputs();
-  clearErrors();
+  clearAll();
   status.value = 'i';
 }
 
 function handleSalvaClick(event) {
   event.preventDefault();
+  const answer = window.confirm('Confirma salvar contato "'+contato.value.nome+'"?')
+  if (answer) {
+    if (contato.value.id == -1) {
+      doAddContato();
+    }
+  }
 }
 
 function handleCancelaClick(event) {
   event.preventDefault();
-  clearInputs();
-  clearErrors();
+  clearAll();
   status.value = 'b';
 }
 
 function handleEditaClick(event) {
   event.preventDefault();
   const index = event.target.value;
-  const answer = window.confirm('Confirma apagar '+index+'?')
-  // clearInputs();
-  // clearErrors();
-  // status.value = 'e';
+  setInputs(props.contato_set[index]);
+  status.value = 'e';
 }
 
 function handleApagaClick(event) {
   event.preventDefault();
   const index = event.target.value;
   const contato_selecionado = props.contato_set[index];
-  const answer = window.confirm('Confirma apagar "'+contato_selecionado.nome+'"?')
+  const answer = window.confirm('Confirma apagar contato "'+contato_selecionado.nome+'"?')
   if (answer) doDelContato(index, contato_selecionado.id);
 }
 
 // Lifecycle Hooks
 
 onMounted(() => {
-  clearInputs();
-  clearErrors();
+  clearAll();
+})
+
+// watch
+watch(status, (newStatus) => {
+  clearAcoesMensagem();
+  if (newStatus != 'b') {
+    inputNomeFocus();
+  }
 })
 
 </script>
@@ -111,12 +185,16 @@ onMounted(() => {
           <th>Preferencial</th>
           <th>Ações</th>
         </tr>
+        <tr v-if="acoes_mensagem">
+          <th colspan="5">{{ acoes_mensagem }}</th>
+        </tr>
         <tr>
           <th>
             <span class="text-sm text-red-700 font-bold" v-if="contato_erro.nome" >{{ contato_erro.nome }}<br /></span>
             <input
               class="mx-0.5 border border-solid border-slate-500 disabled:border-slate-200 rounded"
               v-model.trim="contato.nome"
+              ref="inputNome"
               type="text"
               size="12"
               name="nome"
