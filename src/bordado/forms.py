668d9f50b45a8c0c7ca53478bef084110eb58ed8
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-04-19 15:01:40 -0300

    FormWidgetAttrs

--- src/bordado/forms.py
+++ src/bordado/forms.py
@@ -2,24 +2,23 @@ from pprint import pprint
 
 from django import forms
 
+from o2lib.form.widget_attrs import FormWidgetAttrs
+
 
 class FinanceiroForm(forms.Form):
+    a = FormWidgetAttrs()
+
     pedido_numero = forms.CharField(
         label='Pedido',
-        widget=forms.TextInput(
-            attrs={
-                'type': 'number',
-                'autofocus': 'autofocus',
-            }
-        ),
         required=False,
+        widget=forms.TextInput(
+            attrs={**a.autofocus, **a.number}
+        ),
     )
     cobranca_id = forms.CharField(
         label='Cobrança',
-        widget=forms.TextInput(
-            attrs={
-                'type': 'number',
-            }
-        ),
         required=False,
+        widget=forms.TextInput(
+            attrs={**a.number}
+        ),
     )
---
+++ src/o2lib/form/widget_attrs.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+
+
+class FormWidgetAttrs():
+    number = {
+        'type': 'number',
+    }
+    string = {
+        'type': 'string',
+    }
+    date = {
+        'type': 'date',
+    }
+    string_upper = {
+        'type': 'string',
+        'style': 'text-transform:uppercase;',
+    }
+    autofocus = {
+        'autofocus': 'autofocus',
+    }
+    placeholder_n0 = {
+        'placeholder': '0',
+    }
+    placeholder_0 = {
+        'placeholder': '0...',
+    }
+    placeholder_0_0 = {
+        'placeholder': '0... 0... ...',
+    }
+    placeholder_item = {
+        'placeholder': '0.00000.000.000000',
+    }
