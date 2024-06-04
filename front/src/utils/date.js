b589b563030b7ceca4e325762276ca564b62460a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-06-04 19:59:46 -0300

    date2InputText passa a utilizar moment format

--- front/src/utils/date.js
+++ front/src/utils/date.js
@@ -18,10 +18,11 @@ export function date2Text(data) {
 }
 
 export function date2InputText(date) {  
-  const year = date.getFullYear()
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const day = String(date.getDate()).padStart(2, '0')
-  return `${year}-${month}-${day}`
+  return moment.utc(date).format('YYYY-MM-DD');
+  // const year = date.getFullYear()
+  // const month = String(date.getMonth() + 1).padStart(2, '0')
+  // const day = String(date.getDate()).padStart(2, '0')
+  // return `${year}-${month}-${day}`
 }
 
 export function inputStrDate2PtBrDate(str_date, empty='') {
