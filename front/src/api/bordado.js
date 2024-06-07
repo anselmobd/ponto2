f146e2ddc2c6901ca2df2d2d09ffb85d575c8412
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-06-06 21:56:39 -0300

    criando getBordado, getBordadoFull e putBordado

--- front/src/api/bordado.js
+++ front/src/api/bordado.js
@@ -25,3 +25,64 @@ export function getBordados(cliente_apelido, callBack) {
     callBack(null, error);
   });
 }
+
+export function getBordado({
+  id=null,
+  callBack=()=>{}
+}) {
+  const params = new URLSearchParams();
+  params.append('format', 'json');
+  axiosPrivate.get(
+    `/bordado/api/bordado/${id}/`,
+    {params: params}
+  )
+  .then(response => {
+    callBack(response.data);
+  })
+  .catch(error => {
+    console.error('Erro ao obter bordado '+id+' via API:', error)
+    callBack(null, error);
+  });
+}
+
+export function getBordadoFull({
+  id=null,
+  callBack=()=>{}
+}) {
+  const params = new URLSearchParams();
+  params.append('format', 'json');
+  axiosPrivate.get(
+    `/bordado/api/bordado__full/${id}/`,
+    {params: params}
+  )
+  .then(response => {
+    callBack(response.data);
+  })
+  .catch(error => {
+    console.error('Erro ao obter bordado '+id+' via API:', error)
+    callBack(null, error);
+  });
+}
+
+export function putBordado({
+  id=null,
+  payload={},  
+  callBack=()=>{}
+}) {
+  const params = new URLSearchParams();
+  params.append('format', 'json');
+  console.log('putBordado', payload);
+  axiosPrivate.put(
+    `/bordado/api/bordado/${id}/`,
+    payload,
+    {params: params}
+  )
+  .then(response => {
+    callBack(response.data);
+  })
+  .catch(error => {
+    console.error('Erro ao gravar dados do bordado '+id+' via API:', error)
+    callBack(null, error);
+  });
+}
+
