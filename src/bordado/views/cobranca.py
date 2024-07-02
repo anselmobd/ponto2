32153928f4cfe609a012402e750045f8297cc948
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-07-02 17:37:54 -0300

    CobrancaView filtra_cliente preve filtrar mais de um cliente

--- src/bordado/views/cobranca.py
+++ src/bordado/views/cobranca.py
@@ -89,16 +89,14 @@ class CobrancaView(LoginRequiredMixin, O2BaseGetPostView):
 
     def filtra_cliente(self):
         if self.cliente_apelido:
-            try:
-                Cliente.objects.get(apelido__icontains=self.cliente_apelido)
-            except Cliente.DoesNotExist:
+            clientes = Cliente.objects.filter(
+                apelido__icontains=self.cliente_apelido)
+            if not clientes:
                 self.form.errors['cliente_apelido'] = [
                     f"Cliente com apelido contendo '{self.cliente_apelido}' "
                     "não existe"]
-                # raise StepErrorException("erro do filtro do cliente")
-            finally:
-                self.query = self.query.filter(
-                    **{f'{self.CLIENTE}__icontains': self.cliente_apelido})
+            self.query = self.query.filter(
+                **{f'{self.CLIENTE}__icontains': self.cliente_apelido})
 
     def context_table(self):
         PrepRows(
