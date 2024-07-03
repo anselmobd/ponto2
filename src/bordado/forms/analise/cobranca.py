be5f1a395f81bfa03eabedcc46206c43909089f2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-07-03 14:49:01 -0300

    AnaliseCobrancaForm filtra por ano e mês

--- src/bordado/forms/analise/cobranca.py
+++ src/bordado/forms/analise/cobranca.py
@@ -3,6 +3,8 @@ from pprint import pprint
 from django import forms
 
 from o2lib.form.widget_attrs import FormWidgetAttrs
+from o2lib.date import ano_atual, mes_atual
+
 
 __all__ = [
     'AnaliseCobrancaForm',
@@ -13,13 +15,25 @@ class AnaliseCobrancaForm(forms.Form):
     a = FormWidgetAttrs()
 
     field_control = [
+        ['ano', 'mes'],
         ['cliente_apelido'],
     ]
+
+    ano = forms.IntegerField(
+        required=False,
+        initial=ano_atual,
+        widget=forms.NumberInput(),
+    )
+
+    mes = forms.IntegerField(
+        required=False,
+        initial=mes_atual,
+    )
 
     cliente_apelido = forms.CharField(
         label='Cliente',
         required=False,
         widget=forms.TextInput(
-            attrs={**a.string}
+            attrs={**a.string, **a.autofocus}
         ),
     )
