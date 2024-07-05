ff74d72a941177aa2afa876c39f88c657e3a9422
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-07-05 20:04:56 -0300

    implementando get_vars2form

--- src/o2lib/views/base/get_post.py
+++ src/o2lib/views/base/get_post.py
@@ -22,6 +22,7 @@ class O2BaseGetPostView(CustomView):
         self.form_dict_initial = {}
         self.form_create_kwargs = {}
         self.get_args2form = True
+        self.get_vars2form = False
         self.cleaned_data2self = False
         self.cleaned_data2context = False
         self.cleaned_data2data = False
@@ -77,11 +78,26 @@ class O2BaseGetPostView(CustomView):
         pass
 
     def get(self, request, *args, **kwargs):
+        print('request')
+        pprint(request.GET)
         self.init_self(request, **kwargs)
+
+        pprint(kwargs)
+        call_post = False
         if self.get_args2form:
             for arg in self.get_args:
                 if self.get_arg(arg) is not None:
-                    return self.post(request, *args, **kwargs)
+                    call_post = True
+        pprint(kwargs)
+        if self.get_vars2form:
+            if request.GET:
+                for key, value in dict(request.GET).items():
+                    kwargs[key] = value[0]
+                    self.get_args.append(key)
+                call_post = True
+        pprint(kwargs)
+        if call_post:
+            return self.post(request, *args, **kwargs)
 
         self.pre_form()
         if self.form_class_has_initial:
