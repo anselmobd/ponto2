eca6f40d5504deed1cdb59b2570b528a01ff91f7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-07-16 16:16:09 -0300

    models: separando PedidoItem

--- src/bordado/models/main.py
+++ src/bordado/models/main.py
@@ -21,6 +21,7 @@ from bordado.models.forma_pagamento import (
     forma_pagamento_default_id,
 )
 from bordado.models.pedido import Pedido
+from bordado.models.pedido_item import PedidoItem
 from bordado.models.tipo_comunicacao import (
     TipoComunicacao,
     tipo_comunicacao_default_id,
@@ -60,110 +61,6 @@ __all__ = [
 #         db_table = 'po2_empresa'
 #         verbose_name = "Empresa"
 #         ordering = ['nome']
-
-
-class PedidoItemManager(models.Manager):
-    def get_by_natural_key(self, ordem, pedido):
-        return self.get(ordem=ordem, pedido__numero=pedido)
-
-
-class PedidoItem(models.Model):
-    admin_order = 500
-    pedido = models.ForeignKey(
-        Pedido,
-        on_delete=models.PROTECT,
-        blank=False,
-        null=False,
-    )
-    ordem = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(100)],
-        default=0,
-    )
-    data_pedido = models.DateField(
-        "Data do pedido",
-        default=timezone.localdate
-    )
-    inserido_em = models.DateTimeField(auto_now_add=True)
-    bordado = models.ForeignKey(
-        Bordado,
-        on_delete=models.PROTECT,
-        blank=False,
-        null=False,
-    )
-    quantidade = models.PositiveIntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(1_000_000)],
-        default=0,
-    )
-    preco = models.DecimalField(
-        "Preço",
-        max_digits=9,
-        decimal_places=2,
-        validators=[
-            MinValueValidator(Decimal(0.01)),
-            MaxValueValidator(Decimal(1_000_000)),
-        ],
-        default=0,
-    )
-    programacao = models.DecimalField(
-        "Pogramação",
-        max_digits=9,
-        decimal_places=2,
-        validators=[
-            MinValueValidator(Decimal(0.01)),
-            MaxValueValidator(Decimal(1_000_000)),
-        ],
-        default=0,
-    )
-    ajuste = models.DecimalField(
-        max_digits=7,
-        decimal_places=2,
-        validators=[
-            MinValueValidator(Decimal(-1_000)),
-            MaxValueValidator(Decimal(1_000)),
-        ],
-        default=0,
-    )
-    cancelado = models.BooleanField(
-        default=False,
-    )
-    usuario = models.ForeignKey(
-        User,
-        on_delete=models.PROTECT,
-        related_name='+',
-        verbose_name="usuário",
-        default=logged_user,
-    )
-
-    objects = PedidoItemManager()
-
-    @property
-    def cliente(self):
-        return self.pedido.cliente
-
-    def __str__(self):
-        return (
-            f"{self.id}: {self.pedido.numero:04d}/{self.ordem} "
-            f"{self.quantidade}*'{self.bordado}'"
-        )
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.ordem = (
-                PedidoItem.objects.filter(pedido=self.pedido).count() + 1
-            ) * 10
-        super(PedidoItem, self).save(*args, **kwargs)
-
-    class Meta:
-        db_table = 'po2_pedido_item'
-        verbose_name = "Item de pedido"
-        verbose_name_plural = "Itens de pedido"
-        ordering = ['-pedido__numero', '-ordem']
-        unique_together = [['ordem', 'pedido']]
-
-    def natural_key(self):
-        return (self.ordem, self.pedido.numero)
-
-    natural_key.dependencies = ['bordado.pedido']
 
 
 class Cobranca(models.Model):
---
+++ src/bordado/models/pedido_item.py
@@ -0,0 +1,123 @@
+from decimal import Decimal
+
+from django.contrib.auth.models import User
+from django.core.validators import (
+    MaxValueValidator,
+    MinValueValidator,
+)
+from django.db import models
+from django.utils import timezone
+
+from o2lib.models.base import logged_user
+
+from bordado.models.bordado import Bordado
+from bordado.models.pedido import Pedido
+
+
+__all__ = [
+    'PedidoItem',
+]
+
+
+class PedidoItemManager(models.Manager):
+    def get_by_natural_key(self, ordem, pedido):
+        return self.get(ordem=ordem, pedido__numero=pedido)
+
+
+class PedidoItem(models.Model):
+    admin_order = 500
+    pedido = models.ForeignKey(
+        Pedido,
+        on_delete=models.PROTECT,
+        blank=False,
+        null=False,
+    )
+    ordem = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)],
+        default=0,
+    )
+    data_pedido = models.DateField(
+        "Data do pedido",
+        default=timezone.localdate
+    )
+    inserido_em = models.DateTimeField(auto_now_add=True)
+    bordado = models.ForeignKey(
+        Bordado,
+        on_delete=models.PROTECT,
+        blank=False,
+        null=False,
+    )
+    quantidade = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(1_000_000)],
+        default=0,
+    )
+    preco = models.DecimalField(
+        "Preço",
+        max_digits=9,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(Decimal(0.01)),
+            MaxValueValidator(Decimal(1_000_000)),
+        ],
+        default=0,
+    )
+    programacao = models.DecimalField(
+        "Pogramação",
+        max_digits=9,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(Decimal(0.01)),
+            MaxValueValidator(Decimal(1_000_000)),
+        ],
+        default=0,
+    )
+    ajuste = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(Decimal(-1_000)),
+            MaxValueValidator(Decimal(1_000)),
+        ],
+        default=0,
+    )
+    cancelado = models.BooleanField(
+        default=False,
+    )
+    usuario = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='+',
+        verbose_name="usuário",
+        default=logged_user,
+    )
+
+    objects = PedidoItemManager()
+
+    @property
+    def cliente(self):
+        return self.pedido.cliente
+
+    def __str__(self):
+        return (
+            f"{self.id}: {self.pedido.numero:04d}/{self.ordem} "
+            f"{self.quantidade}*'{self.bordado}'"
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.ordem = (
+                PedidoItem.objects.filter(pedido=self.pedido).count() + 1
+            ) * 10
+        super(PedidoItem, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'po2_pedido_item'
+        verbose_name = "Item de pedido"
+        verbose_name_plural = "Itens de pedido"
+        ordering = ['-pedido__numero', '-ordem']
+        unique_together = [['ordem', 'pedido']]
+
+    def natural_key(self):
+        return (self.ordem, self.pedido.numero)
+
+    natural_key.dependencies = ['bordado.pedido']
