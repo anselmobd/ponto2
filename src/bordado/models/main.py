829c4dd67185a37ff58a22f67f289b8d3412e29a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-07-16 16:33:02 -0300

    models: separando ApontamentoProducao

--- src/bordado/models/main.py
+++ src/bordado/models/main.py
@@ -12,6 +12,7 @@ from django.utils import timezone
 from o2lib.datetime.tz import tz_local
 from o2lib.models.base import logged_user
 
+from bordado.models.aponta_prod import ApontamentoProducao
 from bordado.models.bordado import Bordado
 from bordado.models.cliente import Cliente
 from bordado.models.cobranca import Cobranca
@@ -65,51 +66,3 @@ __all__ = [
 #         db_table = 'po2_empresa'
 #         verbose_name = "Empresa"
 #         ordering = ['nome']
-
-
-class ApontamentoProducaoManager(models.Manager):
-    def get_by_natural_key(self, apontado_em, op):
-        return self.get(apontado_em=apontado_em, op__numero=op)
-
-
-class ApontamentoProducao(models.Model):
-    admin_order = 1100
-    op = models.ForeignKey(
-        OrdemProducao,
-        on_delete=models.PROTECT,
-        blank=False,
-        null=False,
-    )
-    qtd_perda = models.IntegerField(
-        "quantidade de perda",
-        validators=[MinValueValidator(0), MaxValueValidator(1_000_000)],
-        default=0,
-    )
-    qtd_prod = models.IntegerField(
-        "quantidade produzida",
-        validators=[MinValueValidator(0), MaxValueValidator(1_000_000)],
-        default=0,
-    )
-    apontado_em = models.DateTimeField(auto_now_add=True)
-    encerrado = models.BooleanField(
-        default=False,
-    )
-
-    objects = ApontamentoProducaoManager()
-
-    def __str__(self):
-        return (
-            f"OP {self.op.numero:04d} {self.qtd_prod} ({self.qtd_perda}) "
-            f"{tz_local(self.apontado_em):%d/%m/%Y %H:%M:%S}"
-        )
-    class Meta:
-        db_table = 'po2_aponta_prod'
-        verbose_name = "Apontamento de produção"
-        verbose_name_plural = "Apontamentos de produção"
-        ordering = ['-op_id', 'apontado_em']
-        unique_together = [['apontado_em', 'op']]
-
-    def natural_key(self):
-        return (self.apontado_em, self.op.numero)
-
-    natural_key.dependencies = ['bordado.ordemproducao']
