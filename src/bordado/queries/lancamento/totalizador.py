4f1e96ec26fa1f8dd5f59649c7bad42b1dd41c70
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-08-15 17:53:53 -0300

    criado get_totais_lancamentos

---
+++ src/bordado/queries/lancamento/totalizador.py
@@ -0,0 +1,40 @@
+from decimal import Decimal
+from pprint import pprint
+
+from o2lib.models.dictlist import queryset2dictlist
+
+from bordado.models.lancamento import Lancamento
+
+
+__all__ = ['get_totais_lancamentos']
+
+
+def get_totais_lancamentos(cliente=None):
+    query = Lancamento.objects
+
+    if cliente:
+        query = query.filter(
+            cliente=cliente
+        )
+
+    query = query.values(
+        'id',
+        'cobranca',
+        'valor',
+    )
+
+    pedido_data = queryset2dictlist(query)
+
+    zero = Decimal('0.00')
+    totais = {
+        'cobrado': zero,
+        'recebido': zero,
+    }
+
+    for row in pedido_data:
+        if row['cobranca'] is None:
+            totais['recebido'] += row['valor']
+        else:
+            totais['cobrado'] += -row['valor']
+
+    return totais
