8816c35992d44ce50187baf182a6ff77a1891427
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-08-23 20:01:07 -0300

    def form_report: baseado no field_control, monta estrutura para utilizar no form_report.html

---
+++ src/o2lib/form/form_report.py
@@ -0,0 +1,29 @@
+from pprint import pprint
+
+
+__all__ = [
+    'form_report',
+]
+
+
+def form_report(form, form_report_excludes=[]):
+    if hasattr(form, 'field_control'):
+        field_control = form.field_control
+    else:
+        field_control = [[key] for key in form.fields.keys()]
+
+    result = []
+    for lin in field_control:
+        line = {}
+        for col in lin:
+            if col in form_report_excludes:
+                continue
+            value = form.cleaned_data[col]
+            if form.fields[col].widget.input_type == 'select':
+                value = dict(form.fields[col].choices)[value]
+            if value:
+                line[form.fields[col].label] = value
+        if line:
+            result.append(line)
+
+    return result
