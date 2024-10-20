5dcb220cd1ba9125c9db63e3eb8b451fff748792
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-10-20 18:39:32 -0300

    criado first_user_id

--- src/o2lib/models/base.py
+++ src/o2lib/models/base.py
@@ -2,6 +2,8 @@ from pprint import pprint
 
 from o2lib.classes.logged_in_user import SingletonLoggedInUser
 
+from django.contrib.auth.models import User
+
 
 __all__ = [
     'logged_user',
@@ -10,3 +12,11 @@ __all__ = [
 
 def logged_user():
     return SingletonLoggedInUser().user
+
+
+def first_user():
+    return User.objects.first()
+
+
+def first_user_id():
+    return first_user().id
