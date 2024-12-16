572437df46fd6b2b2628e732e12b2e77fca754f5
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-12-15 21:29:47 -0300

    criada useFinanceiroVueStore: uma store no Pinia para indicar necessidade de atualização de componentes da tela Financeiro

---
+++ front/src/stores/financeiro.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+
+export const useFinanceiroVueStore = defineStore('financeiro_vue', {
+  state: () => ({
+    precisaRecarregar: false,
+    componentesRegistrados: {},
+    componentesRecarregados: new Set(),
+  }),
+  actions: {
+    ativarRecarregar() {
+      this.precisaRecarregar = true;
+      this.componentesRecarregados.clear();
+    },
+    registrarComponente(id) {
+      // Adiciona registro do filho ao montar
+      this.componentesRegistrados[id.name] = id.params;
+      this.componentesRegistrados[id.name].concluido = false;
+    },
+    removerRegistroComponente(id) {
+      // Remove o registro do filho ao desmontar
+      delete this.componentesRegistrados[id.name];
+    },
+    componenteConcluiuRecarregar(id) {
+      if (this.componentesRegistrados[id.name]) {
+        this.componentesRegistrados[id.name].concluido = true;
+        this.componentesRecarregados.add(id.name);
+      }
+      if (Object.values(this.componentesRegistrados).every(filho => filho.concluido)) {
+        this.precisaRecarregar = false;
+      }
+    },
+  },
+});
