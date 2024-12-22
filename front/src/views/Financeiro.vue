4394ef7282e6702bb22c772b2e06db761ec6d9ea
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-12-22 17:11:16 -0300

    passa a utilizar o componente lista_lancamentos

--- front/src/views/Financeiro.vue
+++ front/src/views/Financeiro.vue
@@ -4,12 +4,13 @@ import { useRoute } from "vue-router";
 import { useFinanceiroVueStore } from '../stores/financeiro.js';
 import { ref, onMounted, watch } from 'vue'
 import { getPedidoItens } from '../api/pedidoItem.js';
-import { getCobrancas, addCobranca } from '../api/cobranca.js';
-import { getLancamentos, addLancamento } from '../api/lancamento.js';
+import { addCobranca } from '../api/cobranca.js';
+import { addLancamento } from '../api/lancamento.js';
 import { getTiposComunicacao } from '../api/tipo_comunicacao.js';
 import { inputStrDate2PtBrDate, date2InputText } from "../utils/date.js";
 import { ptBrCurrencyFormat } from "../utils/numStr.js";
 import lista_cobrancas from '../components/financeiro/lista_cobrancas.vue';
+import lista_lancamentos from '../components/financeiro/lista_lancamentos.vue';
 
 const route = useRoute();
 
@@ -23,17 +24,6 @@ const pedido_itens_error = ref(null)
 
 const tipo_comunicacao = ref([])
 const tipo_comunicacao_error = ref(null)
-
-const lancamentos = ref([])
-const lancamentos_carregando = ref(1)
-// 0: carregado
-// 1: carregando quantidade padrão
-// 2: carregando mais quantidade padrão
-// 3: carregando até último em aberto
-const lancamentos_error = ref(null)
-const tem_next_page = ref(null)
-const lancamentos_ultima_data = ref(null)
-const lancamentos_ultimo_id = ref(null)
 
 // variaveis comuns
 
@@ -132,7 +122,6 @@ function cbAddCobranca(data, error) {
     clearComunicado();
     doGetPedidoItens();
     financeiroVueStore.ativarRecarregar();
-    doGetLancamentos(1);
   }
   if (error) {
     comunicado.value.error = error.response.data.human.join('|');
@@ -165,61 +154,11 @@ function doAddCobranca(callBack) {
   });
 }
 
-function cbGetLancamentos(data, error) {
-  if (data?.results) {
-    if ([1, 3].includes(lancamentos_carregando.value)) {
-      lancamentos.value = data.results;
-    } else if (lancamentos_carregando.value == 2) {
-      lancamentos.value = lancamentos.value.concat(data.results);
-    }
-    if (lancamentos_carregando.value == 3) {
-      // se "carregando até último em aberto" não sabemos se tem próxima
-      // página, então assume que tem
-      tem_next_page.value = 2;
-    } else {
-      tem_next_page.value = data.next;
-    }
-    if (data.results.length) {
-      const ultimoLancamento = data.results[data.results.length - 1];
-      lancamentos_ultima_data.value = ultimoLancamento.data;
-      lancamentos_ultimo_id.value = ultimoLancamento.id;
-    }
-  }
-  if (error) {
-    lancamentos_error.value = error;
-  };
-  lancamentos_carregando.value = 0;
-}
-
-function doGetLancamentos(carregando) {
-  lancamentos_carregando.value = carregando;
-  if ([1, 3].includes(lancamentos_carregando.value)) {
-    lancamentos.value = [];
-    lancamentos_ultima_data.value = null;
-    lancamentos_ultimo_id.value = null;
-  }
-  let page_size = null;
-  let ate_ultimo_aberto = null;
-  if (lancamentos_carregando.value == 3) {
-    page_size = 999999;
-    ate_ultimo_aberto = 's';
-  }
-  lancamentos_error.value = null;
-  getLancamentos({
-    page_size: page_size,
-    cliente_apelido: route.params.apelido,
-    ultima_data: lancamentos_ultima_data.value,
-    ultimo_id: lancamentos_ultimo_id.value,
-    ate_ultimo_aberto: ate_ultimo_aberto,
-    callBack: cbGetLancamentos
-  });
-}
-
 function cbAddLancamento(data, error) {
   if (data) {
     status.value = 'b';
     clearLancamento();
-    doGetLancamentos(1);
+    financeiroVueStore.ativarRecarregar('lancamento');
   }
   if (error) {
     lancamento.value.error = error.response.data.human.join('|');
@@ -284,21 +223,6 @@ function handleSalvaLancamentoClick(event) {
   doAddLancamento();
 }
 
-function handleRecarregaLancamentosClick(event) {
-  event.preventDefault();
-  doGetLancamentos(1);
-}
-
-function handleMaisLancamentosClick(event) {
-  event.preventDefault();
-  doGetLancamentos(2);
-}
-
-function handleLancamentosEmAbertoClick(event) {
-  event.preventDefault();
-  doGetLancamentos(3);
-}
-
 function handleConciliaCobrancaClick(event) {
   event.preventDefault();
   router.push({
@@ -313,7 +237,6 @@ onMounted(() => {
   doGetTiposComunicacao();
   doGetPedidoItens();
   financeiroVueStore.ativarRecarregar();
-  doGetLancamentos(1);
 })
 
 </script>
@@ -606,132 +529,9 @@ onMounted(() => {
 
     <section id="lista_lancamentos">
       <h3 class="my-4 font-bold text-lg text-center">Lançamentos</h3>
-      <table class="w-full">
-        <thead>
-          <tr>
-            <th>Nº</th>
-            <th>Data</th>
-            <th>Informação</th>
-            <th>Comunicação</th>
-            <th>NF</th>
-            <th>Cobrança</th>
-            <th>Parcela</th>
-            <th>Valor</th>
-            <th>Status</th>
-            <th>Saldo</th>
-          </tr>
-          <tr v-if="lancamentos_error">
-            <th class="text-red-800" colspan="8">
-              {{ lancamentos_error }}
-            </th>
-          </tr>
-          <tr v-if="!lancamentos_carregando && (lancamentos.length == 0)">
-            <th colspan="10">Nenhum lançamento encontrado</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="lancamento in lancamentos"
-            :key="lancamento.id"
-          >
-            <td>{{ lancamento.id }}</td>
-            <td>{{ inputStrDate2PtBrDate(lancamento.data) }}</td>
-            <td>{{ lancamento?.cobranca ? lancamento.cobranca.informacao : lancamento.informacao }}</td>
-            <td>{{ lancamento?.cobranca?.comunicacao?.descricao ? lancamento.cobranca.comunicacao.descricao : '-' }}</td>
-            <td>{{ lancamento?.cobranca?.nf ? lancamento.cobranca.nf : '-' }}</td>
-            <td>{{ lancamento?.cobranca?.id ? lancamento.cobranca.id : '-' }}</td>
-            <td>{{ lancamento?.n_parcelas > 1 ? lancamento.parcela+'/'+lancamento.n_parcelas : lancamento?.n_parcelas == 1 ? 'única' : '-' }}</td>
-            <td class="!text-right"
-              :class="{
-                'text-red-800': lancamento.cobranca,
-                'text-green-800': !lancamento.cobranca,
-              }"
-            >{{ ptBrCurrencyFormat.format(lancamento.valor) }}</td>
-            <td
-              :class="{
-                'text-orange-500':
-                  ( ( lancamento.cobranca &&
-                      ( lancamento.valor_total_recebido > 0 ) &&
-                      ( lancamento.valor_total_recebido < -lancamento.valor )
-                    ) ||
-                    ( !lancamento.cobranca &&
-                      ( lancamento.valor_total_pago > 0 ) &&
-                      ( lancamento.valor_total_pago < lancamento.valor )
-                    )
-                  ),
-                'text-red-800':
-                  ( lancamento.cobranca && 
-                    ( lancamento.valor_total_recebido == 0 )
-                  ) ||
-                  ( !lancamento.cobranca &&
-                    ( lancamento.valor_total_pago == 0 )
-                  ),
-                'text-green-800':
-                  ( ( lancamento.cobranca && 
-                      ( lancamento.valor_total_recebido == 
-                        -lancamento.valor
-                      )
-                    ) ||
-                    ( !lancamento.cobranca &&
-                      ( lancamento.valor_total_pago == 
-                        lancamento.valor
-                      )
-                    )
-                  ),
-              }"
-            >
-            <!-- rec {{ lancamento.valor_total_recebido }}|
-            pag {{ lancamento.valor_total_pago }}|
-            val {{ lancamento.valor }}|
-            val+rec {{ lancamento.valor + lancamento.valor_total_recebido }}|
-            val-pag {{ lancamento.valor - lancamento.valor_total_pago }}|
-            rec_0{{( lancamento.valor_total_recebido == 0 )}}
-            rec_full{{( lancamento.valor_total_recebido == -lancamento.valor )}}
-            rec_parc{{( ( lancamento.valor_total_recebido > 0 ) &&
-                      ( lancamento.valor_total_recebido < -lancamento.valor ) )}} -->
-            {{ lancamento.cobranca ?
-              ( lancamento.valor_total_recebido != 0 ? (
-                  lancamento.valor_total_recebido - lancamento.cobranca?.valor == 0
-                  ? 'Recebido'
-                  : 'Parcial'
-                ) : 'Aberto'
-              ) :
-              ( lancamento.valor_total_pago != 0 ? (
-                  lancamento.valor_total_pago - lancamento.valor == 0
-                  ? 'Conciliado'
-                  : 'Parcial'
-                ) : 'Aberto'
-              )
-            }}</td>
-            <td class="!text-right">{{ ptBrCurrencyFormat.format(lancamento.saldo_cliente) }}</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr v-if="lancamentos_carregando == 1">
-            <td colspan="10">Carregando lançamentos...</td>
-          </tr>
-          <tr v-if="lancamentos_carregando == 2">
-            <td colspan="10">Carregando mais lançamentos...</td>
-          </tr>
-          <tr v-if="lancamentos_carregando == 3">
-            <td colspan="10">Carregando lançamentos até último em aberto...</td>
-          </tr>
-        </tfoot>
-      </table>
 
-      <button
-        v-if="!lancamentos_carregando"
-        @click="handleMaisLancamentosClick"
-        :disabled="tem_next_page == null"
-      >Mais lançamentos</button>
-      <button
-        v-if="!lancamentos_carregando"
-        @click="handleLancamentosEmAbertoClick"
-      >Até último em aberto</button>
-      <button
-        v-if="!lancamentos_carregando"
-        @click="handleRecarregaLancamentosClick"
-      >Recarrega</button>
+      <lista_lancamentos/>
+
       <p>&nbsp;</p>
       <p class="text-center mb-4">
         <button
